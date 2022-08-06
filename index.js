f433c5e94638d15d2b3fa56724ab7c4e082de1d1
anannanur <anannaanu974@gmail.com>
2022-08-06 17:42:52 +0600

    all done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 // connecting database
-const uri = "mongodb+srv://ananna:GLxy1oaAb2GafPdq@cluster0.va7ke.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.va7ke.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -49,20 +49,19 @@ async function run() {
         // editing a task
         app.put("/edit-task/:id", async (req, res) => {
             const {id} = req.params;
-
             const user = req.body;
-
             const query = { _id: ObjectId(id) };
             const options = { upsert: true };
             const info = {
-                $set: {
-                    
+                $set: {                 
                     name: user.name,
                     des: user.des,
+                    date: user.date,
+                    time: user.time,
+                    email : user.email
                 },
             };
             const result = await taskCollection.updateOne(query, info, options);
-
             res.send(result);
         });
 
